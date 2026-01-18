d520dc096c608ac6e8099eb2d2fac0ac243a4c4a
warathep-704 <warathep.kj@gmail.com>
2026-01-19 05:23:55 +0700

    setup

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,17 +2,49 @@ package server
 
 import (
 	"nextzy-spinner-backend/internal/database"
+	"os"
+	"strings"
+	"time"
 
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func Start() {
-	r := gin.Default()
+	godotenv.Load()
+	ginApp := gin.New()
 
 	db := database.Connect()
 	defer database.Disconnect()
 
-	RegisterRoutes(r, db)
+	RegisterRoutes(ginApp, db)
 
-	r.Run(":8080")
+	originsEnv := os.Getenv("SERVER_CORS_ALLOW_ORIGIN")
+	ServerCorsAllowOrigin := strings.Split(originsEnv, ",")
+
+	ginApp.RemoveExtraSlash = true
+	ginApp.Use(gin.Recovery())
+	ginApp.Use(gin.Logger())
+	ginApp.Use(cors.New(cors.Config{
+		AllowOriginFunc: func(origin string) bool {
+			for _, allowed := range ServerCorsAllowOrigin {
+				if allowed == "*" || allowed == origin {
+					return true
+				}
+			}
+			return false
+		},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	ginApp.Run(":" + port)
 }
